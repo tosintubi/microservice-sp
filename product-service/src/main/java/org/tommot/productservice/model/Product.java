4c6dbd15916c78597db0898f499751c9ccebd3bd
tosintubi <tosintubi@users.noreply.github.com>
2021-03-25 00:01:38 +0100

    Id annotations for data object

--- product-service/src/main/java/org/tommot/productservice/model/Product.java
+++ product-service/src/main/java/org/tommot/productservice/model/Product.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @Data
 public class Product {
 
+    @Id
     private String id;
     private String name;
     private String description;
