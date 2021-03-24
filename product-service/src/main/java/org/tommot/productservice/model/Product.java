9a6408e903ccb17f368b8f8dfd540798274379b8
tosintubi <tosintubi@users.noreply.github.com>
2021-03-25 00:00:52 +0100

    lombok annotations for data object

--- product-service/src/main/java/org/tommot/productservice/model/Product.java
+++ product-service/src/main/java/org/tommot/productservice/model/Product.java
@@ -1,11 +1,19 @@
 package org.tommot.productservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
 
 @Document(value = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class Product {
 
     private String id;
