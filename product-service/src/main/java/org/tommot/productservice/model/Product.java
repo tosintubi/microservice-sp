d3e20f4509886c0f60a5d34a8e54170322b6ffae
tosintubi <tosintubi@users.noreply.github.com>
2021-03-24 23:59:46 +0100

    data object mongo mapping

--- product-service/src/main/java/org/tommot/productservice/model/Product.java
+++ product-service/src/main/java/org/tommot/productservice/model/Product.java
@@ -1,4 +1,15 @@
 package org.tommot.productservice.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+
+@Document(value = "product")
 public class Product {
+
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 }
