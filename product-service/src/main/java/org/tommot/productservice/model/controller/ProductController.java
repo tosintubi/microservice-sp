6bba9a8f3e3c0ae9a0474dec50f99a4be9cf55b1
tosintubi <tosintubi@users.noreply.github.com>
2021-03-25 00:12:30 +0100

    ProductController: createProduct method

--- product-service/src/main/java/org/tommot/productservice/model/controller/ProductController.java
+++ product-service/src/main/java/org/tommot/productservice/model/controller/ProductController.java
@@ -2,10 +2,8 @@ package org.tommot.productservice.model.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.tommot.productservice.model.Product;
 import org.tommot.productservice.model.repository.ProductRepository;
 
@@ -23,5 +21,11 @@ public class ProductController {
     @ResponseStatus(HttpStatus.OK)
     public List<Product> findAllProducts(){
         return productRepository.findAll();
+    }
+
+
+    @PostMapping("/create/")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        return new ResponseEntity<>(product, HttpStatus.OK);
     }
 }
