1c17968ba9174a94583851f9d9d4416a7e798dde
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-04-10 22:08:46 +0200

    security config for API gateway

--- api-gateway-service/src/main/java/org/tommot/apigatewayservice/SecurityConfig.java
+++ api-gateway-service/src/main/java/org/tommot/apigatewayservice/SecurityConfig.java
@@ -2,20 +2,21 @@ package org.tommot.apigatewayservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity){
-        httpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-                .oauth2Login(Customizer.withDefaults());
-        httpSecurity.csrf().disable();
-        return httpSecurity.build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .oauth2Login(withDefaults());
+        http.csrf().disable();
+        return http.build();
     }
 }
