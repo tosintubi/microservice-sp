9b4f25abfa6830f38d7c34877a2ecb3893bea136
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-04-16 23:17:12 +0200

    created inventory Class and added cloud boostrarter to pom.xml

---
+++ inventory-service/src/main/java/org/tommot/inventoryservice/model/Inventory.java
@@ -0,0 +1,23 @@
+package org.tommot.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "inventory")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer stock;
+
+}
