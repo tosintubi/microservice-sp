44c567100d09db9fdc6f70e1341ffab4bdaba488
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-04-18 14:09:15 +0200

    refactored getStockBalance and added Query annotation

--- inventory-service/src/main/java/org/tommot/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/org/tommot/inventoryservice/repository/InventoryRepository.java
@@ -1,6 +1,7 @@
 package org.tommot.inventoryservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.tommot.inventoryservice.model.Inventory;
 
 import java.util.List;
@@ -8,5 +9,8 @@ import java.util.Optional;
 
 public interface InventoryRepository  extends JpaRepository<Inventory, Long> {
     Optional<Inventory> findBySkuCode(String skuCode);
-    //List<Inventory> getStockBalance(String skuCode);
+
+
+    @Query("SELECT SUM(i.stock) from Inventory i WHERE i.skuCode = 'skuCode'")
+    Integer getStockBalance(String skuCode);
 }
