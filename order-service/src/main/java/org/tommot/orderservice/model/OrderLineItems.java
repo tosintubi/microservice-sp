0377309a0ca0a91607e26207e03b1492cf745ba7
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-04-18 15:24:07 +0200

    OrderLineItems model

--- order-service/src/main/java/org/tommot/orderservice/model/OrderLineItems.java
+++ order-service/src/main/java/org/tommot/orderservice/model/OrderLineItems.java
@@ -1,4 +1,18 @@
 package org.tommot.orderservice.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
 public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
 }
